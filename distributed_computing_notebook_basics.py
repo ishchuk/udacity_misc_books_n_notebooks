20e1c6b6532ebe0e23f6d21b3588f06860e5f414
Lapuafox <eugen.istschuk@gmail.com>
2020-05-09 10:35:02 +0200

    Update distributed_computing_notebook_basics.py

--- distributed_computing_notebook_basics.py
+++ distributed_computing_notebook_basics.py
@@ -46,3 +46,23 @@ if __name__ == "__main__":
 #run spark job from your terminal
 spark-submit --master local[4] /home/repl/spark-script.py
 #here we are having a local cluster of 4 instances
+
+
+#_________________________________________________________________________
+#basic DAG for Airflow (example from DataCamp)
+
+# Create the DAG object
+dag = DAG(dag_id="car_factory_simulation",
+          default_args={"owner": "airflow","start_date": airflow.utils.dates.days_ago(2)},
+          schedule_interval="0 * * * *")
+
+# Task definitions
+assemble_frame = BashOperator(task_id="assemble_frame", bash_command='echo "Assembling frame"', dag=dag)
+place_tires = BashOperator(task_id="place_tires", bash_command='echo "Placing tires"', dag=dag)
+assemble_body = BashOperator(task_id="assemble_body", bash_command='echo "Assembling body"', dag=dag)
+apply_paint = BashOperator(task_id="apply_paint", bash_command='echo "Applying paint"', dag=dag)
+
+# Complete the downstream flow
+assemble_frame.set_downstream(place_tires)
+assemble_frame.set_downstream(assemble_body)
+assemble_body.set_downstream(apply_paint)
