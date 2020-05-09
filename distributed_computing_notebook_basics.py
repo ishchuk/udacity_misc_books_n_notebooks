e1b53dff326947a3e309179c0f358beb0191bbda
Lapuafox <eugen.istschuk@gmail.com>
2020-05-09 09:52:19 +0200

    Create distributed_computing_notebook_basics.py

---
+++ distributed_computing_notebook_basics.py
@@ -0,0 +1,10 @@
+from multiprocessing import Pool
+# Function to apply a function over multiple cores
+@print_timing
+def parallel_apply(apply_func, groups, nb_cores):
+    with Pool(nb_cores) as p:
+        results = p.map(apply_func, groups)
+    return pd.concat(results)
+
+# Parallel apply using 1 core
+parallel_apply(take_mean_age, athlete_events.groupby('Year'), 1)
