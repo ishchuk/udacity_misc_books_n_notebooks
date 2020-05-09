3bfc78aa96fa7917644e436381fbe5412010d2b9
Lapuafox <eugen.istschuk@gmail.com>
2020-05-09 10:20:34 +0200

    Update distributed_computing_notebook_basics.py

--- distributed_computing_notebook_basics.py
+++ distributed_computing_notebook_basics.py
@@ -8,3 +8,41 @@ def parallel_apply(apply_func, groups, nb_cores):
 
 # Parallel apply using 1 core
 parallel_apply(take_mean_age, athlete_events.groupby('Year'), 1)
+
+#_______________________________________________________________________
+#same but in dask
+
+import dask.dataframe as dd
+athlete_dask_df = dd.from_pandas(athlete_df, npartitions=4)
+
+#testing
+athlete_dask_df.groupby('Year').mean().compute()
+
+#_______________________________________________________________________
+#basic spark code should by put into .py file
+
+from pyspark.sql import SparkSession
+
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.getOrCreate()
+    athlete_events_spark = (spark
+        .read
+        .csv("/home/repl/datasets/athlete_events.csv",
+             header=True,
+             inferSchema=True,
+             escape='"'))
+
+    athlete_events_spark = (athlete_events_spark
+        .withColumn("Height",
+                    athlete_events_spark.Height.cast("integer")))
+
+    print(athlete_events_spark
+        .groupBy('Year')
+        .mean('Height')
+        .orderBy('Year')
+        .show())
+    
+#run spark job from your terminal
+spark-submit --master local[4] /home/repl/spark-script.py
+#here we are having a local cluster of 4 instances
